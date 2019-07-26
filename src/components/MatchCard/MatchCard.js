c9b59b516d36c8940b1d4dfb13119fd63ed25cd9
Ghost Ghost <ghost77@Ghosts-MacBook-Pro.local>
2019-07-26 15:35:32 -0700

    fixed click

--- src/components/MatchCard/MatchCard.js
+++ src/components/MatchCard/MatchCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./MatchCard.css";
 
 const MatchCard = props => (
-    <div onclick={() => props.setClicked(props.id)} className="card col-md-3">
+    <div onClick={() => props.setClicked(props.id)} className="card col-md-3">
     <div className="img-container">
         <img alt={props.name} src={props.image} />
 
