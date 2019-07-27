8535ca6a0900818e811d2997d39c8bcce3a869fd
Ghost Ghost <ghost77@Ghosts-MacBook-Pro.local>
2019-07-27 12:09:50 -0700

    Updates

---
+++ precache-manifest.c28cf535c41fc3ae938d788411e23487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bc7d6f6018fe9b1937c210519e0361",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "082555ecfdbc1ca8f0bf",
+    "url": "/Clicky-Game/static/css/main.d5d53c87.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "082555ecfdbc1ca8f0bf",
+    "url": "/Clicky-Game/static/js/main.a71847e3.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
